2d60a3406e6ab0645a189ba3450a06d0352259ae
JulianMayorga <mayorga.julian@gmail.com>
2013-07-25 11:38:33 -0300

    Inicializar repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonitoreoWeb::Application.config.secret_key_base = '79802a4b594fa67fee22a0f02658916fd040aff55e40fc10a68af0e7140dc7e44c8f21b697e4063e7024d764e56a5125b8380123bba4c57de731d74288d8d505'
