ed72eab4b760f122c5239e4ce3a55e26f6a179ea
JulianMayorga <mayorga.julian@gmail.com>
2013-07-25 13:40:32 -0300

    Agregar memoria y almacenamiento, tanto usado como total de los servidores

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,48 @@
 class Server < ActiveRecord::Base
+  require 'snmp'
+
+  def total_memory
+    result = ""
+    begin
+      result = manager.get(['hrStorageSize.1']).varbind_list[0].value
+    rescue
+      result = "Error"
+    end
+    result
+  end
+
+  def used_memory
+    result = ""
+    begin
+      result = manager.get(['hrStorageUsed.1']).varbind_list[0].value
+    rescue
+      result = "Error"
+    end
+    result
+  end
+
+  def total_storage
+    result = ""
+    begin
+      result = manager.get(['hrStorageSize.31']).varbind_list[0].value
+    rescue
+      result = "Error"
+    end
+    result
+  end
+
+  def used_storage
+    result = ""
+    begin
+      result = manager.get(['hrStorageUsed.31']).varbind_list[0].value
+    rescue
+      result = "Error"
+    end
+    result
+  end
+
+  private
+  def manager
+    @manager = SNMP::Manager.new(:host => ip, :community => 'public', :MibModules => ['HOST-RESOURCES-MIB'])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725144756) do
+ActiveRecord::Schema.define(version: 20130725145244) do
+
+  create_table "metrics", force: true do |t|
+    t.integer  "total_memory"
+    t.integer  "used_memory"
+    t.integer  "total_storage"
+    t.integer  "used_storage"
+    t.integer  "server_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "servers", force: true do |t|
     t.string   "ip"
