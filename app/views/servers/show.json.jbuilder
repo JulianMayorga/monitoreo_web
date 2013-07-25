e05fecba5ad14beb8edc9055dd899cbf1f2bb8c3
JulianMayorga <mayorga.julian@gmail.com>
2013-07-25 11:49:04 -0300

    Agregar scaffold de servidores

---
+++ app/views/servers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @server, :ip, :created_at, :updated_at
---
+++ db/migrate/20130725144756_create_servers.rb
@@ -0,0 +1,9 @@
+class CreateServers < ActiveRecord::Migration
+  def change
+    create_table :servers do |t|
+      t.string :ip
+
+      t.timestamps
+    end
+  end
+end
